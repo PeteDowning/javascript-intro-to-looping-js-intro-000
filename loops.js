2cc9bf40a99b8f5136b452fc6ab28c36bed7cd9f
Peter Downing <petedowning14@gmail.com>
2018-10-24 16:18:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,7 +12,10 @@ function whileLoop(number) {
 }
 
 function doWhileLoop(array) {
-  
+  do {
+    array.pop()
+  } while (array.length > 0 && incrementVariable());
+  return array
 }
 
 function incrementVariable() {
